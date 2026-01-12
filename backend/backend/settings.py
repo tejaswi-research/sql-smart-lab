258f11c0d71d80bfb1f743974e90f6f09adb5128
Sai Tejaswi Lanka <tejaswi3598@example.com>
2026-01-12 18:11:32 +0530

    Fix: Added Render domain to ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-le3u@=@7)($7z*@uk%px)q-z7%7@*jbv)gwirb%xxqd=m4$j8y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sql-smart-lab.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
